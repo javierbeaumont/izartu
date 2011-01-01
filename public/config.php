789ca08e737e050acd6635fa598455ca275eabbb
Javier Beaumont <contact@javierbeaumont.org>
2011-01-01 11:18:56 +0100

    Added initial structure files

---
+++ public/config.php
@@ -0,0 +1,28 @@
+<?php
+
+#  Izartu
+#
+#  Copyright © 2011 Javier Beaumont <contact@javierbeaumont.org>
+#
+#  This file is part of Izartu.
+#
+#  Izartu is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  Izartu is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Izartu. If not, see <http://www.gnu.org/licenses/>.
+
+define('DEBUG', TRUE);
+
+/**
+ * @file config.php
+ * @brief Site configuration file.
+ */
+?>
