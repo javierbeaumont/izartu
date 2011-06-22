73452fa8c4dd86f47105d75fafefdfc201968932
Javier Beaumont <543017+javierbeaumont@users.noreply.github.com>
2011-06-22 23:29:12 +0200

    Added login form

---
+++ private/template/default/login.php
@@ -0,0 +1,31 @@
+<?php
+
+#  Izartu
+#
+#  Copyright © 2011 Javier Beaumont <contact@javierbeaumont.org>
+#
+#  This file is part of Izartu.
+#
+#  Izartu is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  Izartu is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Izartu. If not, see <http://www.gnu.org/licenses/>.
+
+?>
+
+        <form>
+          <fieldset>
+            <legend><?php echo __('Login'); ?></legend>
+            <div><label for="iam">Email</label> <input type="text" id="iam" name="iam"/></div>
+            <div><label for="dro">Password</label> <input type="password" id="dro" name="dro"/></div>
+            <div><input type="submit" id="login" name="login" value="<?php echo __('Login'); ?>"/></div>
+          </fieldset>
+        </form>
