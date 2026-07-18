61d114cb695429246f10282eaaf4cc75641d4112
Javier Beaumont <javierbeaumont@users.noreply.github.com>
2026-07-18 09:13:39 +0200

    Update the AGPLv3 license and file headers

--- private/class/Benchmark.php
+++ private/class/Benchmark.php
@@ -1,22 +1,22 @@
 <?php
-#  Izartu
+#  izartu: web bookmark manager based on tags
 #
-#  Copyright © 2011-2012 Javier Beaumont <contact@javierbeaumont.org>
+#  Copyright (C) 2011-2026 Javier Beaumont <javierbeaumont@users.noreply.github.com>
 #
-#  This file is part of Izartu.
+#  This file is part of izartu.
 #
-#  Izartu is free software: you can redistribute it and/or modify
+#  izartu is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Affero General Public License as
 #  published by the Free Software Foundation, either version 3 of the
 #  License, or (at your option) any later version.
 #
-#  Izartu is distributed in the hope that it will be useful,
+#  izartu is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #  GNU Affero General Public License for more details.
 #
 #  You should have received a copy of the GNU Affero General Public License
-#  along with Izartu. If not, see <http://www.gnu.org/licenses/>.
+#  along with izartu. If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * @file benchmark.php
